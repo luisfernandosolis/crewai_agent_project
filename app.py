8f27e62502a6242408951803b9ac2c9a9dd1edce
Luis Fernando Solís Navarro <lsolis@prediqtdata.com>
2025-05-24 00:11:13 -0500

    Automatically generated commit message based on the code changes

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 import streamlit as st
 
-# Set title
-st.title('Este es mi segundo app con streamlit')
-st.markdown('<style>h1{color: blue;}</style>', unsafe_allow_html=True)
+# Set page title
+st.title('Este es mi segundo app con streamil')
+st.markdown('<style>h1 {color: blue;}</style>', unsafe_allow_html=True)
 
-# Create a sidebar with a red background
-st.sidebar.markdown('<style>div{background-color: red;}</style>', unsafe_allow_html=True)
+# Sidebar with red background
+st.sidebar.markdown('<style>h1 {color: red;}</style>', unsafe_allow_html=True)
 
-# Add text input for user's name
-user_name = st.text_input('Escribe tu nombre aquí')
+# Text input for user's name
+user_name = st.text_input('Escribe tu nombre')
 
-# Add button to submit the name
-submit_btn = st.button('Enviar', key='submit_btn')
-
-# Display welcome message with user's name
-if submit_btn:
+# Button to submit name
+if st.button('Enviar', key='submit_button'):
     st.write(f'Bienvenido, {user_name}!')
