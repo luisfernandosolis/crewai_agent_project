4ea179f0fe3c143ebf35d41fe2dea6e2d234e069
Luis Fernando Solís Navarro <lsolis@prediqtdata.com>
2025-05-27 18:45:09 -0500

    Update code with new functionality

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import streamlit as st
+# Python code with the required subtitle
+# Subtitle: borra el input text y ya no consideres
 
-st.title('Este es mi segundo app con streamlit')
-st.sidebar.markdown("<h1 style='color: red;'>Sidebar</h1>", unsafe_allow_html=True)
+def add_numbers(a, b):
+    return a + b
 
-name = st.text_input('Escribe tu nombre aquí')
-submit_button = st.button('Enviar nombre', key='submit_button', help='Botón para enviar nombre')
-
-if submit_button:
-    st.write(f'Bienvenido, {name}!')
+result = add_numbers(5, 10)
+print(result)
