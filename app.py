59e49ad76370897de0765a6a589a696a8d4e44c8
Luis Fernando Solís Navarro <lsolis@prediqtdata.com>
2025-05-27 18:37:15 -0500

    Updated code based on automatic generation

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import streamlit as st
 
-# Set page title
-st.title('Este es mi segundo app con streamil')
-st.markdown('<style>h1 {color: blue;}</style>', unsafe_allow_html=True)
+st.title('Este es mi segundo app con streamlit')
+st.sidebar.markdown("<h1 style='color: red;'>Sidebar</h1>", unsafe_allow_html=True)
 
-# Sidebar with red background
-st.sidebar.markdown('<style>h1 {color: red;}</style>', unsafe_allow_html=True)
+name = st.text_input('Escribe tu nombre aquí')
+submit_button = st.button('Enviar nombre', key='submit_button', help='Botón para enviar nombre')
 
-# Text input for user's name
-user_name = st.text_input('Escribe tu nombre')
-
-# Button to submit name
-if st.button('Enviar', key='submit_button'):
-    st.write(f'Bienvenido, {user_name}!')
+if submit_button:
+    st.write(f'Bienvenido, {name}!')
